445308bd429474fab47a47bf8c5f962c5bc4aa3a
Peter Wilmott <p@p8952.info>
2014-11-28 20:51:23 +0000

    Use DATABASE_URL

--- lib/models.rb
+++ lib/models.rb
@@ -1,4 +1,9 @@
-DB = Sequel.connect(ENV['DATABASE_URL'] || 'sqlite://db/database.sqlite3')
+if ENV['DATABASE_URL'].nil?
+	puts 'DATABASE_URL is not set!'
+	exit
+end
+
+DB = Sequel.connect(ENV['DATABASE_URL'])
 
 class Attacks < Sequel::Model
 end
